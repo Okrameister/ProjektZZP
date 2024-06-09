5bdbccefcfcdcca448f9b310e123e281fb42777e
Okrameister <dawid.kob02@gmail.com>
2024-06-09 22:13:27 +0200

    CRUD status

--- src/main/java/com/zawadzkia/springtodo/task/status/TaskStatusController.java
+++ src/main/java/com/zawadzkia/springtodo/task/status/TaskStatusController.java
@@ -1,26 +1,60 @@
 package com.zawadzkia.springtodo.task.status;
 
-import com.zawadzkia.springtodo.task.TaskDTO;
 import com.zawadzkia.springtodo.task.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/task/status")
+@RequestMapping("/status")
 @RequiredArgsConstructor
 public class TaskStatusController {
 
-    private final TaskService taskService;
+    private final TaskStatusService taskStatusService;
 
-    @PostMapping(value = "/{id}")
-    String updateTask(@PathVariable Long id, @ModelAttribute("status") TaskStatusDTO taskStatusDTO) {
-        TaskDTO taskDTO = taskService.getTaskDTOById(id);
-        taskDTO.setStatus(taskStatusDTO);
-        taskService.update(taskDTO);
-        return "task/list";
+    @GetMapping
+    String getStatusList(Model model) {
+        List<TaskStatusDTO> statusList = taskStatusService.getUserTaskStatusList();
+        model.addAttribute("statuses", statusList);
+        return "task/statuses/list";
+    }
+
+    @GetMapping("/create")
+    public ModelAndView createStatus() {
+        ModelAndView model = new ModelAndView("/task/statuses/createStatus");
+        model.addObject("status", new TaskStatusModel());
+        return model;
+    }
+
+    @PostMapping("/create")
+    public String saveStatus(@ModelAttribute TaskStatusModel Status) {
+        taskStatusService.saveStatus(Status);
+        return "redirect:/status";
+    }
+
+    @GetMapping("/edit/{statusId}")
+    public ModelAndView editStatus(@PathVariable Long statusId) {
+        ModelAndView model = new ModelAndView("/task/statuses/editStatus");
+        TaskStatusModel status = taskStatusService.getStatusModelById(statusId);
+        model.addObject("status", status);
+        return model;
+    }
+
+    @PostMapping("/edit/{statusId}")
+    public String saveEditedStatus(
+            @PathVariable Long statusId,
+            @ModelAttribute TaskStatusModel status) {
+        taskStatusService.editStatus(statusId, status);
+        return "redirect:/status";
+    }
+
+    @GetMapping("/delete/{statusId}")
+    public String deleteStatus(@PathVariable Long statusId) {
+        taskStatusService.deleteStatusById(statusId);
+        return "redirect:/status";
     }
 }
--- src/main/java/com/zawadzkia/springtodo/task/status/TaskStatusService.java
+++ src/main/java/com/zawadzkia/springtodo/task/status/TaskStatusService.java
@@ -1,6 +1,8 @@
 package com.zawadzkia.springtodo.task.status;
 
 import com.zawadzkia.springtodo.auth.AppUserDetails;
+import com.zawadzkia.springtodo.user.UserModel;
+import com.zawadzkia.springtodo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import java.util.List;
 public class TaskStatusService {
 
     private final TaskStatusRepository taskStatusRepository;
+    private final UserRepository userRepository;
 
     public List<TaskStatusDTO> getUserTaskStatusList() {
         ArrayList<TaskStatusDTO> result = new ArrayList<>();
@@ -24,5 +27,30 @@ public class TaskStatusService {
         }
         return result;
     }
+    public TaskStatusModel getStatusModelById(Long id) {
+        TaskStatusModel StatusModel = taskStatusRepository.getReferenceById(id);
+        return StatusModel;
+    }
 
+    public void saveStatus(TaskStatusModel Status) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserModel user = userRepository.findByUsername(username).orElseThrow();
+        Status.setOwner(user);
+        taskStatusRepository.save(Status);
+    }
+
+    public void editStatus(Long StatusId, TaskStatusModel updatedTaskStatus) {
+        TaskStatusModel Status = taskStatusRepository.findById(StatusId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid task ID: " + StatusId));
+        Status.setName(updatedTaskStatus.getName());
+        taskStatusRepository.save(Status);
+    }
+
+    public void deleteStatusById(Long StatusId) {
+        try {
+            taskStatusRepository.deleteById(StatusId);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }
